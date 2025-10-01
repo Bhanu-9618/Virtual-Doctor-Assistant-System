8807b27b26a993088713287f13b9201fd08a5458
Nipun Bhanuka <nipunbhanuka12@gmail.com>
2025-10-01 15:43:32 +0530

    Update app3.js

--- app3.js
+++ app3.js
@@ -1,5 +1,5 @@
 
-const API_KEY = "AIzaSyBz9IoqaSmkEdYRQeGIvY3UQwtUbM8mGWs"; 
+const API_KEY = "_API_KEY_"; 
 const MODEL = "gemini-1.5-flash"; 
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${MODEL}:generateContent?key=${API_KEY}`;
 
